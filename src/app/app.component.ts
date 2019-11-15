6c8aa9a8d3d46f3414aba7679864b90839740270
Siriwasan <siriwasan@gmail.com>
2019-11-15 23:31:47 +0700

    implement pdfMake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdf-export';
+
+  constructor() {
+  }
+
+  exportPDF() {
+    const docDefinition = {
+      content: [{ text: 'สวัสดีประเทศไทย reat pdf demo ', fontSize: 15 }]
+    };
+    pdfMake.createPdf(docDefinition).open();
+  }
 }
