816edde684893e7eadb69578ec1c2daec01e0201
Siriwasan <siriwasan@gmail.com>
2019-11-16 21:53:13 +0700

    add fontello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 const pdf = pdfMake;
 pdf.vfs = pdfFonts.pdfMake.vfs;
 
-// const or = pdf.PageOrientation;
-
 pdf.fonts = {
   THSarabunNew: {
     normal: 'THSarabunNew.ttf',
@@ -20,6 +18,12 @@ pdf.fonts = {
     bold: 'Roboto-Medium.ttf',
     italics: 'Roboto-Italic.ttf',
     bolditalics: 'Roboto-MediumItalic.ttf'
+  },
+  Fontello: {
+    normal: 'fontello.ttf',
+    bold: 'fontello.ttf',
+    italics: 'fontello.ttf',
+    bolditalics: 'fontello.ttf'
   }
 };
 
@@ -31,22 +35,70 @@ pdf.fonts = {
 export class AppComponent {
   title = 'pdf-export';
 
-  constructor() {
-    // console.log(or);
-  }
+  constructor() {}
 
   exportPDF() {
     const docDefinition: pdfMake.TDocumentDefinitions = {
       content: [
-        { text: 'สวัสดีประเทศไทย reat pdf demo ', fontSize: 15 },
+        { text: 'สวัสดีประเทศไทย reat pdf demo ', style: 'thai' },
         { ul: ['item 1', 'item 2'], type: 'square' },
-        { qr: 'Hello! world' }
+        { qr: 'Hello! world' },
+        { text: [{ text: '', style: 'icon' }, ' icon-check'] },
+        { text: [{ text: '', style: 'icon' }, ' icon-check-empty'] },
+        { text: [{ text: '', style: 'icon' }, ' icon-circle-empty'] },
+        { text: [{ text: '', style: 'icon' }, ' icon-dot-circled'] },
+        {
+          text: 'This is a header, using header style',
+          style: 'header'
+        },
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam.\n\n',
+        {
+          text: 'Subheader 1 - using subheader style',
+          style: 'subheader'
+        },
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
+        {
+          text: 'Subheader 2 - using subheader style',
+          style: 'subheader'
+        },
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
+        {
+          text:
+            'It is possible to apply multiple styles, by passing an array. This paragraph uses two styles: quote and small. When multiple styles are provided, they are evaluated in the specified order which is important in case they define the same properties',
+          style: ['quote', 'small']
+        }
       ],
-      defaultStyle: {
-        font: 'THSarabunNew'
+      styles: {
+        icon: {
+          font: 'Fontello'
+        },
+        thai: {
+          font: 'THSarabunNew',
+          fontSize: 15
+        },
+        header: {
+          fontSize: 18,
+          bold: true
+        },
+        subheader: {
+          fontSize: 15,
+          bold: true
+        },
+        quote: {
+          italics: true
+        },
+        small: {
+          fontSize: 8
+        }
       },
-      pageOrientation: pdfMake.PageOrientation.LANDSCAPE,
-      pageSize: pdfMake.PageSize.A5
+      // defaultStyle: {
+      //   font: 'THSarabunNew'
+      // },
+      pageOrientation: pdfMake.PageOrientation.PORTRAIT,
+      pageSize: pdfMake.PageSize.A4
     };
     pdfMake.createPdf(docDefinition).open();
   }
